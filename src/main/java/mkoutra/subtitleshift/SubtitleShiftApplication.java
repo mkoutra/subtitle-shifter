b8e430a5eb87392eb60c9af49fc316d063f01e69
mkoutra <koutra98@hotmail.com>
2025-01-25 18:49:08 +0200

    Initial commi: Empty project

---
+++ src/main/java/mkoutra/subtitleshift/SubtitleShiftApplication.java
@@ -0,0 +1,13 @@
+package mkoutra.subtitleshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtitleShiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubtitleShiftApplication.class, args);
+    }
+
+}
